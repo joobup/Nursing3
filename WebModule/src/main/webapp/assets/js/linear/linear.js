805f7bf1806780803fc1ef70ba3dbefa99f5bfd7
gengjiajun <915522005@qq.com>
2016-11-23 15:07:13 +0800

    报表样式改动

--- WebModule/src/main/webapp/assets/js/linear/linear.js
+++ WebModule/src/main/webapp/assets/js/linear/linear.js
@@ -11,14 +11,14 @@ var myData = {
             fillColor : "rgba(220,220,220,.5)",
             strokeColor : "rgba(220,220,220,1)",
             pointColor : "rgba(220,220,220,1)",
-            pointStrokeColor : "#fff",
+            pointStrokeColor : "#ffffff",
             data : [65,59,90,81,56,55,40]
         },
         {
             fillColor : "rgba(90,190,90,.5)",
             strokeColor : "rgba(90,190,90,1)",
             pointColor : "rgba(90,190,90,1)",
-            pointStrokeColor : "#fff",
+            pointStrokeColor : "#ffffff",
             data : [40,48,40,40,90,27,90]
         }
     ]
--- WebModule/src/main/webapp/common/serveHead.ftl
+++ WebModule/src/main/webapp/common/serveHead.ftl
@@ -1,6 +1,6 @@
 <div class="head-banner">
     <div id="logo">
-        <img src="${domainUrl}/assets/images/index/logo1.png">
+        <a href="${domainUrl}"><img src="${domainUrl}/assets/images/index/logo2.png"></a>
     </div>
     <div id="user-status">
         <tui>[ 退出 ]</tui> 欢迎您：<b>管理员</b>
